e5fe01d6ec2eb36971d01534672f89acb19e5c90
JuanCeja <jmc347ls2@yahoo.com>
2022-06-30 17:42:32 -0700

    initial commit

---
+++ singly-linked-list.js
@@ -0,0 +1,57 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    isEmpty() {
+        if (this.head) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+
+    // when a pointer is to the LEFT of an equal sign, we are CHANGING it
+    // when a pointer is to the RIGHT of an equal sign, we are READING it
+
+    // add given node to the head, if it exists. return void
+    // list is empty?
+    // list already has nodes?
+    addToFront(node) {
+        if (this.node == node) {
+            return "void"
+        } else {
+            this.node = node
+        }
+    }
+
+    // create a new node with given data, add it to the head. return void
+    addDataToFront(data) { }
+}
+
+// instantiate the SLL
+var myCoolSLL = new SLL();
+
+// creating nodes
+// var myNode = new Node(55);
+
+// executing methods:
+myCoolSLL.addToFront(new Node(55));
+myCoolSLL.addToFront(new Node(100));
+console.log(myCoolSLL);
+
+
+myCoolSLL.addToFront(new Node(100));
+myCoolSLL.addToFront(new Node(44));
+myCoolSLL.addToFront(new Node(33));
+myCoolSLL.addToFront(new Node(22));
+
+
